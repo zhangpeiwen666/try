a0fe26ad15022d7959163584e6ec0a4f7cbce94d
zhangpeiwen666 <896603863@qq.com>
2018-06-19 16:30:01 +0800

    添加methodA，methodB方法

--- GitDemo/src/com/dzqc/test/Test.java
+++ GitDemo/src/com/dzqc/test/Test.java
@@ -4,5 +4,19 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("测试!");
 	}
+	/**
+	 * 添加methodA方法
+	 */
+	
+	public void methodA() {
+		
+	}
+	/**
+	 * 添加methodA方法
+	 */
+	
+	public void methodB() {
+		
+	}
 
 }
